87416ed90549f6168edddd11ca95a3414023eb69
Anoop puri <anoop.puri@qtsolv.com>
2026-07-22 15:05:09 +0530

    update herosetting for deploy

--- app/Settings/HeroSettings.php
+++ app/Settings/HeroSettings.php
@@ -6,14 +6,14 @@ use Spatie\LaravelSettings\Settings;
 
 class HeroSettings extends Settings
 {
-    public string $eyebrow;
-    public string $headline_line1;
-    public string $headline_line2;
-    public string $headline_line3;
+    public ?string $eyebrow = null;
+    public ?string $headline_line1 = null;
+    public ?string $headline_line2 = null;
+    public ?string $headline_line3 = null;
     public ?string $subheading = null;
-    public string $cta_primary_text;
+    public ?string $cta_primary_text = null;
     public ?string $cta_primary_url = null;
-    public string $cta_secondary_text;
+    public ?string $cta_secondary_text = null;
     public ?string $cta_secondary_url = null;
     public ?string $video_url = null;
 
