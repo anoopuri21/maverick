8c8f8dcc0d5b6b8fe0d8299269639d9adf79960a
Anoop puri <anoop.puri@qtsolv.com>
2026-07-22 16:28:35 +0530

    Add: spatie/laravel-data dependency

--- app/Settings/SiteSettings.php
+++ app/Settings/SiteSettings.php
@@ -8,10 +8,10 @@ class SiteSettings extends Settings
 {
     public ?string $logo_url = null;
     public ?string $logo_white_url = null;
-    public string $phone;
-    public string $whatsapp_number;
-    public string $email;
-    public string $address;
+    public ?string $phone = null;
+    public ?string $whatsapp_number = null;
+    public ?string $email = null;
+    public ?string $address = null;
     public ?string $facebook_url = null;
     public ?string $instagram_url = null;
     public ?string $linkedin_url = null;
